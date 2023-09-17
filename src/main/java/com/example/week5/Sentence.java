5bc8d56aa16ef831cafb7d6ef7dbdde6e9a67b62
Urangotan-pure <orangutan11112000@gmail.com>
2023-09-17 11:07:56 +0700

    Initial commit

---
+++ src/main/java/com/example/week5/Sentence.java
@@ -0,0 +1,10 @@
+package com.example.week5;
+
+import java.util.ArrayList;
+
+public class Sentence {
+
+    //ชนิดข้อมูลแบบ ArrayList และมีการทำ Generic เป็น String
+    public ArrayList<String> badsentences =  new ArrayList<String>();
+    public ArrayList<String> goodsentences =  new ArrayList<String>();
+}
---
+++ src/main/java/com/example/week5/SentenceConsumer.java
@@ -0,0 +1,28 @@
+package com.example.week5;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SentenceConsumer {
+
+    protected Sentence sentence;
+
+    public SentenceConsumer(){
+        sentence = new Sentence();
+    }
+
+    @RabbitListener(queues = "GoodWordQueue")
+    public void addGoodSentences(String sentence){
+        this.sentence.goodsentences.add(sentence);
+//        System.out.println("AddGood : " + sentence);
+        System.out.println("In addGoodSentence Methods : " + this.sentence.goodsentences);
+    }
+
+    @RabbitListener(queues = "BadWordQueue")
+    public void addbadSentences(String sentence){
+        this.sentence.badsentences.add(sentence);
+//        System.out.println("AddBad : " + sentence);
+        System.out.println("In addBadSentence Methods : " + this.sentence.badsentences);
+    }
+}
